922ca2d8e150570b4478441baed69c221f62b53b
matzefratze123 <matzefratze123@web.de>
2014-04-14 23:48:51 +0200

    Add better parent exception handling

--- src/main/java/de/matzefratze123/heavyspleef/stats/AccountException.java
+++ src/main/java/de/matzefratze123/heavyspleef/stats/AccountException.java
@@ -5,9 +5,14 @@ public class AccountException extends Exception {
 	private static final long	serialVersionUID	= 3259591029892667831L;
 	
 	private String detailMessage;
+	private Exception parent;
 	
 	public AccountException(String message) {
 		this.detailMessage = message;
+	}
+	
+	public AccountException(Exception parent) {
+		this.parent = parent;
 	}
 	
 	public AccountException() {}
@@ -15,6 +20,15 @@ public class AccountException extends Exception {
 	@Override
 	public String getMessage() {
 		return detailMessage == null ? super.getMessage() : detailMessage;
+	}
+	
+	@Override
+	public void printStackTrace() {
+		if (parent != null) {
+			parent.printStackTrace();
+		} else {
+			super.printStackTrace();
+		}
 	}
 	
 }
