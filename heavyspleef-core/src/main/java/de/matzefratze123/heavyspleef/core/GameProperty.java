eb65063298c39cc5b2782de25582de3cb841be00
matzefratze123 <matzefratze123@web.de>
2014-12-23 15:27:04 +0100

    Add various game properties

--- heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/GameProperty.java
+++ heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/GameProperty.java
@@ -3,7 +3,14 @@ package de.matzefratze123.heavyspleef.core;
 public enum GameProperty {
 	
 	INSTANT_BREAK(true),
-	JOIN_ON_COUNTDOWN(true);
+	PLAY_BLOCK_BREAK(true),
+	JOIN_ON_COUNTDOWN(true),
+	DISABLE_HUNDER(true),
+	DISABLE_PVP(true),
+	SUPPRESS_ENTITY_SPAWNING(true),
+	DISABLE_ITEM_PICKUP(true),
+	DISABLE_ITEM_DROP(true),
+	DISABLE_BUILD(true);
 	
 	private Object defaultValue;
 	
