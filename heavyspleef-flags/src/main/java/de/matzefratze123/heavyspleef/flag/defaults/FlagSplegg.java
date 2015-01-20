dd51ab9a172fcb430aba1335bb46abe8f5650ff3
matzefratze123 <matzefratze123@web.de>
2015-01-20 18:56:44 +0100

    Add Splegg flag

---
+++ heavyspleef-flags/src/main/java/de/matzefratze123/heavyspleef/flag/defaults/FlagSplegg.java
@@ -0,0 +1,130 @@
+package de.matzefratze123.heavyspleef.flag.defaults;
+
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.projectiles.ProjectileSource;
+import org.bukkit.util.BlockIterator;
+
+import com.google.common.collect.Lists;
+
+import de.matzefratze123.heavyspleef.core.Game;
+import de.matzefratze123.heavyspleef.core.GameProperty;
+import de.matzefratze123.heavyspleef.core.GameState;
+import de.matzefratze123.heavyspleef.core.event.GameListener;
+import de.matzefratze123.heavyspleef.core.event.GameStartEvent;
+import de.matzefratze123.heavyspleef.core.player.SpleefPlayer;
+import de.matzefratze123.heavyspleef.flag.presets.BooleanFlag;
+
+public class FlagSplegg extends BooleanFlag {
+
+	private static final String SPLEGG_LAUNCHER_DISPLAYNAME = ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Splegg Launcher";
+	private static final List<String> SPLEGG_LAUNCHER_LORE = Lists.newArrayList(ChatColor.GRAY + "Right-Click to launch an egg");
+	private static final ItemStack SPLEGG_LAUNCHER_ITEMSTACK;
+	
+	static {
+		SPLEGG_LAUNCHER_ITEMSTACK = new ItemStack(Material.IRON_SPADE);
+		
+		ItemMeta meta = SPLEGG_LAUNCHER_ITEMSTACK.getItemMeta();
+		meta.setDisplayName(SPLEGG_LAUNCHER_DISPLAYNAME);
+		meta.setLore(SPLEGG_LAUNCHER_LORE);
+		
+		SPLEGG_LAUNCHER_ITEMSTACK.setItemMeta(meta);
+	}
+	
+	@Override
+	public void defineGameProperties(Map<GameProperty, Object> properties) {
+		properties.put(GameProperty.DISABLE_FLOOR_BREAK, true);
+	}
+
+	@Override
+	public boolean hasGameProperties() {
+		return true;
+	}
+
+	@Override
+	public boolean hasBukkitListenerMethods() {
+		return true;
+	}
+
+	@Override
+	public void getDescription(List<String> description) {
+		description.add("Enables the Splegg gamemode in spleef games.");
+	}
+	
+	@SuppressWarnings("deprecation")
+	@GameListener
+	public void onGameStart(GameStartEvent event) {
+		Game game = event.getGame();
+		
+		for (SpleefPlayer player : game.getPlayers()) {
+			Inventory inv = player.getBukkitPlayer().getInventory();
+			inv.addItem(SPLEGG_LAUNCHER_ITEMSTACK);
+			
+			player.getBukkitPlayer().updateInventory();
+		}
+	}
+	
+	@EventHandler
+	public void onProjectileHit(ProjectileHitEvent event) {
+		if (!getValue()) {
+			return;
+		}
+		
+		Projectile projectile = event.getEntity();
+		if (!(projectile instanceof Egg)) {
+			return;
+		}
+		
+		ProjectileSource source = projectile.getShooter();
+		
+		if (!(source instanceof Player)) {
+			return;
+		}
+		
+		SpleefPlayer shooter = getHeavySpleef().getSpleefPlayer(source);
+		Game game = getHeavySpleef().getGameManager().getGame(shooter);
+		
+		if (game == null || game.getGameState() != GameState.INGAME) {
+			return;
+		}
+		
+		// Use a BlockIterator to determine where the arrow has hit the ground
+		BlockIterator blockIter = new BlockIterator(projectile.getWorld(), 
+				projectile.getLocation().toVector(), 
+				projectile.getVelocity().normalize(), 
+				0, 4);
+		
+		Block blockHit = null;
+		
+		while (blockIter.hasNext()) {
+			blockHit = blockIter.next();
+			
+			if (blockHit.getType() != Material.AIR) {
+				break;
+			}
+		}
+		
+		if (!game.canSpleef(blockHit)) {
+			//Cannot remove this block
+			return;
+		}
+		
+		projectile.remove();
+		game.addBlockBroken(shooter, blockHit);
+		
+		blockHit.setType(Material.AIR);
+	}
+	
+}
