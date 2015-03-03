76044775ca0e7acf66fb227ff6844ee7d1379d2b
matzefratze123 <matzefratze123@web.de>
2015-03-03 12:06:02 +0100

    Add some license headers again

--- heavyspleef-flags/src/main/java/de/matzefratze123/heavyspleef/flag/defaults/FlagSpectate.java
+++ heavyspleef-flags/src/main/java/de/matzefratze123/heavyspleef/flag/defaults/FlagSpectate.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of HeavySpleef.
+ * Copyright (c) 2014-2015 matzefratze123
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.matzefratze123.heavyspleef.flag.defaults;
 
 import java.util.List;
@@ -22,7 +39,7 @@ import de.matzefratze123.heavyspleef.core.player.PlayerStateHolder;
 import de.matzefratze123.heavyspleef.core.player.SpleefPlayer;
 import de.matzefratze123.heavyspleef.flag.presets.LocationFlag;
 
-@Flag(name = "spawnpoint")
+@Flag(name = "spectate")
 public class FlagSpectate extends LocationFlag {
 	
 	private Set<SpleefPlayer> spectators;
--- heavyspleef-flags/src/main/java/de/matzefratze123/heavyspleef/flag/defaults/FlagTimeout.java
+++ heavyspleef-flags/src/main/java/de/matzefratze123/heavyspleef/flag/defaults/FlagTimeout.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of HeavySpleef.
+ * Copyright (c) 2014-2015 matzefratze123
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.matzefratze123.heavyspleef.flag.defaults;
 
 import java.util.List;
@@ -11,10 +28,12 @@ import de.matzefratze123.heavyspleef.core.event.GameEndEvent;
 import de.matzefratze123.heavyspleef.core.event.GameListener;
 import de.matzefratze123.heavyspleef.core.event.GameListener.Priority;
 import de.matzefratze123.heavyspleef.core.event.GameStartEvent;
+import de.matzefratze123.heavyspleef.core.flag.Flag;
 import de.matzefratze123.heavyspleef.core.i18n.I18N;
 import de.matzefratze123.heavyspleef.core.i18n.Messages;
 import de.matzefratze123.heavyspleef.flag.presets.IntegerFlag;
 
+@Flag(name = "timeout")
 public class FlagTimeout extends IntegerFlag {
 
 	private static final long TICKS_MULTIPLIER = 20L;
