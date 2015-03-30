adbc02f0005718d0e0e8398ff53bc5f1a1e1395e
matzefratze123 <matzefratze123@web.de>
2015-03-30 22:29:26 +0200

    Finish the updateWall() method

--- heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/extension/ExtensionInfoWall.java
+++ heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/extension/ExtensionInfoWall.java
@@ -17,7 +17,15 @@
  */
 package de.matzefratze123.heavyspleef.core.extension;
 
+import java.util.Iterator;
+
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.util.BlockIterator;
+import org.bukkit.util.Vector;
 
 import de.matzefratze123.heavyspleef.commands.base.Command;
 import de.matzefratze123.heavyspleef.commands.base.CommandContext;
@@ -29,19 +37,29 @@ import de.matzefratze123.heavyspleef.core.GameManager;
 import de.matzefratze123.heavyspleef.core.HeavySpleef;
 import de.matzefratze123.heavyspleef.core.PlayerPostActionHandler.PostActionCallback;
 import de.matzefratze123.heavyspleef.core.PlayerPostActionHandler.PostActionType;
+import de.matzefratze123.heavyspleef.core.config.ConfigType;
+import de.matzefratze123.heavyspleef.core.config.SignLayoutConfiguration;
 import de.matzefratze123.heavyspleef.core.event.GameListener;
 import de.matzefratze123.heavyspleef.core.event.GameListener.Priority;
+import de.matzefratze123.heavyspleef.core.event.GameEndEvent;
 import de.matzefratze123.heavyspleef.core.event.GameStateChangeEvent;
 import de.matzefratze123.heavyspleef.core.event.PlayerJoinGameEvent;
 import de.matzefratze123.heavyspleef.core.event.PlayerLeaveGameEvent;
-import de.matzefratze123.heavyspleef.core.event.PlayerLoseGameEvent;
 import de.matzefratze123.heavyspleef.core.extension.ExtensionInfoWall.WallValidationException.Cause;
 import de.matzefratze123.heavyspleef.core.i18n.I18N;
 import de.matzefratze123.heavyspleef.core.i18n.Messages;
+import de.matzefratze123.heavyspleef.core.layout.SignLayout;
+import de.matzefratze123.heavyspleef.core.layout.VariableProvider;
 import de.matzefratze123.heavyspleef.core.player.SpleefPlayer;
 
 public class ExtensionInfoWall implements GameExtension {
 
+	private static final String INGAME_PLAYER_PREFIX_KEY = "ingame-player-prefix";
+	private static final String DEAD_PLAYER_PREFIX_KEY = "dead-player-prefix";
+	
+	private static final String DEFAULT_INGAME_PLAYER_PREFIX = "";
+	private static final String DEFAULT_DEAD_PLAYER_PREFIX = ChatColor.GRAY.toString();
+	
 	@Command(name = "addwall", permission = "heavyspleef.addwall", 
 			description = "Adds a wall consisting of signs to show the status of a particular game", 
 			usage = "/spleef addwall <game>", minArgs = 1)
@@ -132,30 +150,97 @@ public class ExtensionInfoWall implements GameExtension {
 		}
 	}
 	
-	@GameListener
+	@GameListener(priority = Priority.MONITOR)
 	public void onGameStateChange(GameStateChangeEvent event) {
 		Game game = event.getGame();
-		
-		updateWall(game);
+		updateWall(game, false);
 	}
 	
 	@GameListener(priority = Priority.MONITOR)
 	public void onPlayerJoin(PlayerJoinGameEvent event) {
-		
+		Game game = event.getGame();
+		updateWall(game, false);
 	}
 	
-	@GameListener
+	@GameListener(priority = Priority.MONITOR)
 	public void onPlayerLeave(PlayerLeaveGameEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
 		
+		Game game = event.getGame();
+		updateWall(game, false);
 	}
 	
-	@GameListener
-	public void onPlayerLose(PlayerLoseGameEvent event) {
-		
+	@GameListener(priority = Priority.MONITOR)
+	public void onGameEnd(GameEndEvent event) {
+		Game game = event.getGame();
+		updateWall(game, true);
 	}
 	
-	private void updateWall(Game game) {
+	private void updateWall(Game game, boolean reset) {
+		World world = game.getWorld();
+		HeavySpleef heavySpleef = game.getHeavySpleef();
+		VariableProvider<Game> varProvider = new GameVariableProvider();
 		
+		SignLayoutConfiguration joinConfig = heavySpleef.getConfiguration(ConfigType.JOIN_SIGN_LAYOUT_CONFIG);
+		SignLayoutConfiguration infoConfig = heavySpleef.getConfiguration(ConfigType.INFO_WALL_SIGN_LAYOUT_CONFIG);
+		
+		String ingamePrefix = infoConfig.getOption(INGAME_PLAYER_PREFIX_KEY, DEFAULT_INGAME_PLAYER_PREFIX);
+		String deadPrefix = infoConfig.getOption(DEAD_PLAYER_PREFIX_KEY, DEFAULT_DEAD_PLAYER_PREFIX);
+		
+		SignLayout joinLayout = joinConfig.getLayout();
+		SignLayout infoLayout = infoConfig.getLayout();
+		
+		//Validate that all blocks are signs
+		Vector startVec = new Vector(start.getBlockX(), start.getBlockY(), start.getBlockZ());
+		Vector endVec = new Vector(end.getBlockX(), end.getBlockY(), end.getBlockZ());
+		Vector dv = endVec.subtract(startVec);
+		
+		Vector directionVec = dv.normalize();
+		
+		int maxDistance = Math.abs(direction == BaseDirection.NORTH || direction == BaseDirection.SOUTH ? endVec.getBlockZ() - startVec.getBlockZ()
+				: endVec.getBlockX() - startVec.getBlockX());
+		BlockIterator iterator = new BlockIterator(world, startVec, directionVec, 0, maxDistance);
+		
+		for (int i = 0; iterator.hasNext(); i++) {
+			Block block = iterator.next();
+			
+			if (!(block instanceof Sign)) {
+				--i;
+				continue;
+			}
+			
+			Sign sign = (Sign) block;
+			
+			Iterator<SpleefPlayer> currentIterator = game.getPlayers().iterator();
+			boolean ingamePlayers = true;
+			
+			if (i == 0) {
+				//First sign is the join sign
+				joinLayout.inflate(sign, varProvider, game);
+			} else if (i == 1) {
+				//Second sign is the informational sign
+				infoLayout.inflate(sign, varProvider, game);
+			} else {
+				for (int j = 0; j < 4; j++) {
+					if (reset) {
+						sign.setLine(j, "");
+						continue;
+					}
+					
+					if (!currentIterator.hasNext()) {
+						currentIterator = game.getDeadPlayers().iterator();
+						ingamePlayers = false;
+					}
+					
+					SpleefPlayer player = currentIterator.next();
+					sign.setLine(j, (ingamePlayers ? ingamePrefix : deadPrefix) + player.getName());
+				}
+				
+				sign.update();
+			}
+		}
 	}
 	
 	public enum BaseDirection {
@@ -189,7 +274,8 @@ public class ExtensionInfoWall implements GameExtension {
 		
 		public enum Cause {
 			
-			NOT_IN_LINE, NOT_SAME_Y_AXIS;
+			NOT_IN_LINE, 
+			NOT_SAME_Y_AXIS;
 			
 		}
 		
