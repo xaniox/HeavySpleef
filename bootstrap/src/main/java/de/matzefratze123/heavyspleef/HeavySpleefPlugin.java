e1d0ee8a735564930f4122ea9ab97e3961e87e47
matzefratze123 <matzefratze123@web.de>
2015-03-31 23:27:44 +0200

    Rename package module to bootstrap

---
+++ bootstrap/src/main/java/de/matzefratze123/heavyspleef/HeavySpleefPlugin.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of HeavySpleef.
+ * Copyright (c) 2014-2015 matzefratze123
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.matzefratze123.heavyspleef;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.matzefratze123.heavyspleef.commands.CommandModule;
+import de.matzefratze123.heavyspleef.core.HeavySpleef;
+import de.matzefratze123.heavyspleef.core.module.Module;
+import de.matzefratze123.heavyspleef.flag.FlagModule;
+import de.matzefratze123.heavyspleef.persistence.PersistenceModule;
+
+public class HeavySpleefPlugin extends JavaPlugin {
+
+	private HeavySpleef heavySpleef;
+	
+	@Override
+	public void onEnable() {
+		heavySpleef = new HeavySpleef(this);
+		heavySpleef.load();
+		
+		Module flagModule = new FlagModule(heavySpleef);
+		Module commandModule = new CommandModule(heavySpleef);
+		Module persistenceModule = new PersistenceModule(heavySpleef);
+		
+		heavySpleef.registerModule(flagModule);
+		heavySpleef.registerModule(commandModule);
+		heavySpleef.registerModule(persistenceModule);
+		
+		heavySpleef.enable();
+	}
+	
+	@Override
+	public void onDisable() {
+		if (heavySpleef != null) {
+			heavySpleef.disable(); 
+		}
+	}
+	
+}
