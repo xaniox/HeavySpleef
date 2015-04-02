4ad27e44e71b3e126944eda19594dd004ce5ed40
matzefratze123 <matzefratze123@web.de>
2015-04-02 16:33:50 +0200

    Fix a bug and provide the is_countdown boolean variable

--- heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/extension/GameVariableProvider.java
+++ heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/extension/GameVariableProvider.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of HeavySpleef.
+ * Copyright (c) 2014-2015 matzefratze123
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.matzefratze123.heavyspleef.core.extension;
 
 import java.util.Set;
@@ -26,6 +43,7 @@ public class GameVariableProvider implements VariableProvider<Game> {
 		vars.add(new Variable("players", game.getPlayers().size()));
 		vars.add(new Variable("dead", game.getDeadPlayers().size()));
 		vars.add(new Variable("countdown", task != null ? task.getRemaining() : 0));
+		vars.add(new Variable("is_countdown", task != null));
 		
 		for (String req : requested) {
 			StringTokenizer tokenizer = new StringTokenizer(req, ":");
@@ -46,12 +64,8 @@ public class GameVariableProvider implements VariableProvider<Game> {
 					vars.add(new Variable(req, game.isFlagPresent(flagPath)));
 				} else if (flagValueRequest) {
 					AbstractFlag<?> flag = game.getFlag(flagPath);
-					if (!game.isFlagPresent(flagPath)) {
-						throw new IllegalArgumentException("Requested flag " + flagPath + " for variable '" + req
-								+ "' not available (Consider using a if check of $[has_flag:" + flagPath + " before trying to get its value");
-					}
 					
-					vars.add(new Variable(req, flag.getValue()));
+					vars.add(new Variable(req, flag != null ? flag.getValue() : null));
 				}
 			}
 		}
