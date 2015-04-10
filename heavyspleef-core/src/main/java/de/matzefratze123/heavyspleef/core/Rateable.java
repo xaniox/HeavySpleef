dbb8c7dca2af610c12120f22da0d8aa4e25ba623
matzefratze123 <matzefratze123@web.de>
2015-04-10 20:38:29 +0200

    Implement the rating feature (like elo)

---
+++ heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/Rateable.java
@@ -0,0 +1,9 @@
+package de.matzefratze123.heavyspleef.core;
+
+public interface Rateable {
+	
+	public double getRating();
+	
+	public void setRating(double rating);
+
+}
---
+++ heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/RatingCompute.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of HeavySpleef.
+ * Copyright (c) 2014-2015 matzefratze123
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.matzefratze123.heavyspleef.core;
+
+import java.util.Map;
+
+import lombok.Getter;
+import de.matzefratze123.heavyspleef.core.player.SpleefPlayer;
+
+public interface RatingCompute {
+	
+	//Leave this as a constant for now
+	public static final int K = 30;
+	public static final int D = 400;
+	
+	public RatingResult compute(Map<String, Statistic> statistics, Game game, SpleefPlayer[] winners);
+	
+	public static class RatingResult {
+		
+		@Getter
+		private Map<String, Double> newRating;
+		
+		public RatingResult(Map<String, Double> newRating) {
+			this.newRating = newRating;
+		}
+		
+	}
+	
+}
