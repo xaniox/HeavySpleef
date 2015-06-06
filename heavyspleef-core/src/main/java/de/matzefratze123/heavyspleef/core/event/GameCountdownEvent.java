86378ea1a3d3ec5a99f3e922beb1f93ed80a14ca
matzefratze123 <matzefratze123@web.de>
2015-06-06 20:44:22 +0200

    Add capability to cancel countdowns

--- heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/event/GameCountdownEvent.java
+++ heavyspleef-core/src/main/java/de/matzefratze123/heavyspleef/core/event/GameCountdownEvent.java
@@ -19,44 +19,26 @@ package de.matzefratze123.heavyspleef.core.event;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import org.bukkit.Location;
 
 import de.matzefratze123.heavyspleef.core.Game;
 
-public class GameCountdownEvent extends GameEvent {
+@Getter @Setter
+public class GameCountdownEvent extends GameEvent implements Cancellable {
 	
+	private boolean cancelled;
 	private List<Location> spawnLocations;
 	private int countdownLength;
 	private boolean countdownEnabled;
+	private String errorBroadcast;
 	
 	public GameCountdownEvent(Game game) {		
 		super(game);
 		
 		countdownEnabled = true;
-	}
-	
-	public void setSpawnLocations(List<Location> spawnLocations) {
-		this.spawnLocations = spawnLocations;
-	}
-	
-	public List<Location> getSpawnLocations() {
-		return spawnLocations;
-	}
-	
-	public void setCountdownLength(int countdownLength) {
-		this.countdownLength = countdownLength;
-	}
-	
-	public int getCountdownLength() {
-		return countdownLength;
-	}
-	
-	public void setCountdownEnabled(boolean countdownEnabled) {
-		this.countdownEnabled = countdownEnabled;
-	}
-	
-	public boolean isCountdownEnabled() {
-		return countdownEnabled;
-	}
+	}	
 
 }
