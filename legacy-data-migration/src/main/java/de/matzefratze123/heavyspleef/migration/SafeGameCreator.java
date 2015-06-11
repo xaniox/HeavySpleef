ef03b4cdf88ed70ea56eef993f27562b1914172f
matzefratze123 <matzefratze123@web.de>
2015-06-11 14:39:53 +0200

    Add a SafeGameCreator to allocate instances without calling the constructor

---
+++ legacy-data-migration/src/main/java/de/matzefratze123/heavyspleef/migration/SafeGameCreator.java
@@ -0,0 +1,168 @@
+/*
+ * This file is part of HeavySpleef.
+ * Copyright (c) 2014-2015 matzefratze123
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.matzefratze123.heavyspleef.migration;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import org.bukkit.World;
+
+import com.google.common.collect.Maps;
+import com.sk89q.worldedit.regions.Region;
+
+import de.matzefratze123.heavyspleef.core.FlagManager;
+import de.matzefratze123.heavyspleef.core.Game;
+import de.matzefratze123.heavyspleef.core.GameState;
+import de.matzefratze123.heavyspleef.core.HeavySpleef;
+import de.matzefratze123.heavyspleef.core.event.EventBus;
+import de.matzefratze123.heavyspleef.core.event.GameEvent;
+import de.matzefratze123.heavyspleef.core.event.SpleefListener;
+import de.matzefratze123.heavyspleef.core.extension.ExtensionManager;
+import de.matzefratze123.heavyspleef.core.floor.Floor;
+
+public class SafeGameCreator {
+	
+	/* Unsafe related variables */
+	private Object theUnsafe;
+	private Method allocateInstanceMethod;
+	
+	/* Game class related variables */
+	private Field nameField;
+	private Field worldField;
+	private Field heavySpleefField;
+	private Field gameStateField;
+	private Field flagManagerField;
+	private Field extensionManagerField;
+	private Field floorsField;
+	private Field deathzonesField;
+	private Field eventBusField;
+	
+	private final HeavySpleef heavySpleef;
+	
+	public SafeGameCreator(HeavySpleef heavySpleef) {
+		this.heavySpleef = heavySpleef;
+		
+		try {
+			Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+			Field theUnsafeField = unsafeClass.getDeclaredField("theUnsafe");
+			theUnsafeField.setAccessible(true);
+			theUnsafe = theUnsafeField.get(null);
+			allocateInstanceMethod = unsafeClass.getDeclaredMethod("allocateInstance", Class.class);
+			allocateInstanceMethod.setAccessible(true);
+		} catch (ClassNotFoundException | NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException
+				| NoSuchMethodException e) {
+			throw new RuntimeException("Could not initialize Unsafe instance", e);
+		}
+		
+		try {
+			Class<Game> gameClass = Game.class;
+			nameField = gameClass.getDeclaredField("name");
+			nameField.setAccessible(true);
+			
+			worldField = gameClass.getDeclaredField("world");
+			worldField.setAccessible(true);
+			
+			heavySpleefField = gameClass.getDeclaredField("heavySpleef");
+			heavySpleefField.setAccessible(true);
+			
+			gameStateField = gameClass.getDeclaredField("gameState");
+			gameStateField.setAccessible(true);
+			
+			flagManagerField = gameClass.getDeclaredField("flagManager");
+			flagManagerField.setAccessible(true);
+			
+			extensionManagerField = gameClass.getDeclaredField("extensionManager");
+			extensionManagerField.setAccessible(true);
+			
+			floorsField = gameClass.getDeclaredField("floors");
+			floorsField.setAccessible(true);
+			
+			deathzonesField = gameClass.getDeclaredField("deathzones");
+			deathzonesField.setAccessible(true);
+			
+			eventBusField = gameClass.getDeclaredField("eventBus");
+			eventBusField.setAccessible(true);
+		} catch (NoSuchFieldException | SecurityException e) {
+			throw new RuntimeException("Could not initialize game field variables. Did the field names change?", e);
+		}
+	}
+	
+	public Game createSafeGame(String name, World world) {
+		Game game;
+		
+		try {
+			game = (Game) allocateInstanceMethod.invoke(theUnsafe, Game.class);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			throw new RuntimeException("Could not allocate a safe instance of Game", e);
+		}
+		
+		try {
+			nameField.set(game, name);
+			worldField.set(game, world);
+			heavySpleefField.set(game, heavySpleef);
+			gameStateField.set(game, GameState.WAITING);
+			
+			Map<String, Floor> floorMap = Maps.newHashMap();
+			Map<String, Region> deathzoneMap = Maps.newHashMap();
+			
+			floorsField.set(game, floorMap);
+			deathzonesField.set(game, deathzoneMap);
+			
+			FlagManager flagManager = new FlagManager(heavySpleef.getPlugin(), null);
+			flagManagerField.set(game, flagManager);
+			
+			EventBus eventBus = new NullEventBus();
+			ExtensionManager extensionManager = new AccessibleExtensionManager(heavySpleef, eventBus);
+			extensionManagerField.set(game, extensionManager);
+			
+			eventBusField.set(game, eventBus);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			throw new RuntimeException("Could not create safe game", e);
+		}
+		
+		return game;
+	}
+	
+	private static class AccessibleExtensionManager extends ExtensionManager {
+
+		public AccessibleExtensionManager(HeavySpleef heavySpleef, EventBus eventManager) {
+			super(heavySpleef, eventManager);
+		}
+		
+	}
+	
+	private static class NullEventBus extends EventBus {
+		
+		protected NullEventBus() {
+			super(null);
+		}
+
+		@Override
+		public void registerListener(SpleefListener listener) {}
+		
+		@Override
+		public void unregister(SpleefListener listener) {}
+		
+		@Override
+		public void callEvent(GameEvent event) {}
+		
+	}
+
+}
